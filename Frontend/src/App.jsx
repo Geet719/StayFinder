bcf6b8639d0abb898bea65c0d30f05de2d5c05bc
Geetanshu <you@example.com>
2025-06-18 11:56:59 +0530

    Fix path error in App.jsx and update Home route

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 import Login from './pages/login.jsx'
 import Signup from './pages/signup'
 import ListingPage1 from './pages/ListingPage1.jsx'
